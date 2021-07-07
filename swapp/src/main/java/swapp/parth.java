25370502ad5fca04d95c3b9d79988c2f5498a53c
swappyy <swapnil-r.kurane@capgemini.com>
2021-07-07 23:53:16 +0530

    yes i am

--- swapp/src/main/java/swapp/parth.java
+++ swapp/src/main/java/swapp/parth.java
@@ -4,7 +4,9 @@ public class parth {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		System.out.println("Hello july");
+		System.out.println("Hello may");
+		System.out.println("Hello june");
 
 	}
 
