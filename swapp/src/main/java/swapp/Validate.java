6f4824ab7a0854dc48126c61bd04ef9238c91aa2
swappyy <swapnil-r.kurane@capgemini.com>
2021-07-08 10:10:55 +0530

    this 2nd

---
+++ swapp/src/main/java/swapp/Validate.java
@@ -0,0 +1,11 @@
+package swapp;
+
+public class Validate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is second code");
+
+	}
+
+}
